d0567912a3e739fcf8ab840714bcfc36f384c5f4
eriksalazar <1eriksalazar@gmail.com>
2017-12-09 23:04:05 -0500

    Various layout changes

---
+++ DesafioWeb/Controllers/EventosController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DesafioWeb.Controllers
+{
+    public class EventosController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ DesafioWeb/Controllers/PredicasController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DesafioWeb.Controllers
+{
+    public class PredicasController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
